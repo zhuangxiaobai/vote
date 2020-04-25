939b6215c323f312acfbdb7c4e477aea24f26172
zhuangxiaobai <584787434@qq.com>
2020-04-26 00:50:50 +0800

    第三次使用测试

--- src/main/java/com/item/vote/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/item/vote/service/serviceImpl/UserServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class UserServiceImpl  implements UserService {
 
     @Autowired
-    private UserMapper userMapper;
+    private  UserMapper userMapper;
 
     @Override
     @Transactional
